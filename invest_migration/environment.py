b2c2d26c12d896ddfe5db29bc57753770d2fc07d
kowalcj0 <kowalczykjanusz@gmail.com>
2018-09-07 17:31:31 +0100

    display response times in the report

--- invest_migration/environment.py
+++ invest_migration/environment.py
@@ -1,3 +1,4 @@
+import json
 import glob
 from typing import List
 
@@ -67,32 +68,73 @@ def get_report_summaries(html_report_file_paths: List[str]) -> List[Summary]:
 
 
 def generate_report_index(summaries: List[Summary]) -> str:
+    totals = {}
+
+    for summary in summaries:
+        default_value = {'total': 0, 'color': summary.color}
+        totals.setdefault(summary.result, default_value)
+        totals[summary.result]['total'] += 1
 
     doc_template = """<html>
     <body>
-        <table style="border: 1px solid black;width:50%;margin-left:25%;margin-right:25%;"> 
+        <table style="border: 1px solid black;width:33%;margin-left:33%;margin-right:33%;"> 
+        <tr>
+            <th style="padding:5px;">Result</th>
+            <th style="padding:5px;">Total</th>
+        </tr>
+        {totals_rows}
+        </table> 
+        <br>
+        <br>
+        <table style="border: 1px solid black;width:70%;margin-left:15%;margin-right:15%;"> 
         <tr>
             <th style="padding:5px;">Result</th>
             <th style="padding:5px;">Report</th>
+            <th style="padding:5px;">{site_a}</th>
+            <th style="padding:5px;">{site_b}</th>
         </tr>
         {rows}
         </table> 
     </body>
     </html>
     """
+    totals_rows = []
+    for key, values in totals.items():
+        result = key
+        color = values['color']
+        total = values['total']
+        row = f"""
+        <tr style="border: 1px solid black;">
+            <td style="padding:5px;background-color:{color};text-align:center">{result}</td>
+            <td>{total}</td>
+        </tr>
+        """
+        totals_rows.append(row)
+
     rows = []
     for summary in summaries:
         file_name = summary.file_name
         result = summary.result
         color = summary.color
+        site_a = summary.site_a
+        site_b = summary.site_b
+        time_a = summary.response_time_a
+        time_b = summary.response_time_b
+        time_a_color = "#00ff80" if time_a < time_b else "#ff0040"
+        time_b_color = "#00ff80" if time_b < time_a else "#ff0040"
         row = f"""
         <tr style="border: 1px solid black;">
             <td style="padding:5px;background-color:{color};text-align:center">{result}</td>
             <td><a href="{file_name}">{file_name}</a></td>
+            <td style="background-color:{time_a_color};text-align:center">{time_a}ms</td>
+            <td style="background-color:{time_b_color};text-align:center">{time_b}ms</td>
         </tr>
         """
         rows.append(row)
-    return doc_template.format(rows="\n\t".join(rows))
+    return doc_template.format(
+        totals_rows="\n\t".join(totals_rows), rows="\n\t".join(rows),
+        site_a=site_a, site_b=site_b, time_a=time_a, time_b=time_b
+    )
 
 
 def save_report_index(html: str):
