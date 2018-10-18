72eb14f299031498ca709880bce724d509d02a17
kowalcj0 <kowalczykjanusz@gmail.com>
2018-10-18 16:31:17 +0100

    configure CMS API Client

--- cms_pages/conftest.py
+++ cms_pages/conftest.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-import pytest
-from directory_api_client.testapiclient import DirectoryTestAPIClient
+from django.conf import settings
 
-from cms_pages import settings
-from cms_pages.settings import CMS_SIGNATURE_SECRET_API_KEY
+from cms_pages.settings import (
+    DIRECTORY_CMS_API_CLIENT_API_KEY,
+    DIRECTORY_CMS_API_CLIENT_BASE_URL,
+)
 
 
-@pytest.fixture
-def cms_client():
-    base_url = settings.CMS_URL
-    return DirectoryTestAPIClient(
-        base_url=base_url, api_key=CMS_SIGNATURE_SECRET_API_KEY
-    )
+settings.configure(
+    DIRECTORY_CMS_API_CLIENT_BASE_URL=DIRECTORY_CMS_API_CLIENT_BASE_URL,
+    DIRECTORY_CMS_API_CLIENT_API_KEY=DIRECTORY_CMS_API_CLIENT_API_KEY,
+    DIRECTORY_CMS_API_CLIENT_SENDER_ID="tests",
+    DIRECTORY_CMS_API_CLIENT_SERVICE_NAME="directory",
+    DIRECTORY_CMS_API_CLIENT_DEFAULT_TIMEOUT=55,
+    DIRECTORY_CMS_API_CLIENT_CACHE_EXPIRE_SECONDS=2592000,
+    CACHES={
+        "cms_fallback": {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "unique-snowflake",
+        }
+    },
+)
