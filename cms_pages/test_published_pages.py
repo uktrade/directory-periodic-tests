9248294c9eebc7f18691f79c52e9b0d980b1fd31
kowalcj0 <kowalczykjanusz@gmail.com>
2018-10-18 16:32:06 +0100

    simpler tests for published & drafts

--- cms_pages/test_published_pages.py
+++ cms_pages/test_published_pages.py
@@ -1,57 +1,31 @@
 # -*- coding: utf-8 -*-
-from pprint import pformat
-
 import pytest
-import requests
 
-from cms_pages import get_page_ids_by_type
-from cms_pages.settings import CMS_API_PAGES_ENDPOINT
+from cms_pages.cms_api_helpers import (
+    find_published_translated_urls,
+    find_published_urls,
+    get_and_assert,
+    invest_find_published_translated_urls,
+    ALL_API_PAGES,
+    INVEST_API_PAGES,
+    NON_INVEST_API_PAGES,
+)
+
+
+@pytest.mark.parametrize("url", find_published_urls(ALL_API_PAGES))
+def test_all_published_english_pages_should_return_200(url):
+    get_and_assert(url, 200)
 
 
 @pytest.mark.parametrize(
-    "page_type",
-    [
-        "export_readiness.GetFinancePage",
-        "export_readiness.PrivacyAndCookiesPage",
-        "export_readiness.TermsAndConditionsPage",
-        "find_a_supplier.IndustryArticlePage",
-        "find_a_supplier.IndustryContactPage",
-        "find_a_supplier.IndustryLandingPage",
-        "find_a_supplier.IndustryPage",
-        "find_a_supplier.LandingPage",
-        "invest.InfoPage",
-        "invest.InvestHomePage",
-        "invest.RegionLandingPage",
-        "invest.SectorLandingPage",
-        "invest.SectorPage",
-        "invest.SetupGuideLandingPage",
-        "invest.SetupGuidePage",
-    ],
+    "url", find_published_translated_urls(NON_INVEST_API_PAGES)
 )
-def test_all_published_pages_should_return_200(cms_client, page_type):
-    results = []
-    page_ids = get_page_ids_by_type(cms_client, page_type)
-    for page_id in page_ids:
-        url = "{}{}/".format(CMS_API_PAGES_ENDPOINT, page_id)
-        try:
-            api_response = cms_client.get(url)
-        except Exception as ex:
-            results.append((page_id, url, str(ex)))
-            continue
-        if api_response.status_code == 200:
-            live_url = api_response.json()["meta"]["url"]
-            try:
-                live_response = requests.get(live_url)
-            except Exception as ex:
-                results.append((page_id, live_url, str(ex)))
-                continue
-            results.append((page_id, live_url, live_response.status_code))
-        else:
-            print("{} returned {}".format(url, api_response.status_code))
-    non_200 = [result for result in results if result[2] != 200]
-    template = "Page ID: {} URL: {} Status Code: {}"
-    formatted_non_200 = [template.format(*result) for result in non_200]
-    error_msg = "{} out of {} published pages of type {} are broken {}".format(
-        len(non_200), len(results), page_type, pformat(formatted_non_200)
-    )
-    assert not non_200, error_msg
+def test_non_invest_published_translated_pages_should_return_200_new(url):
+    get_and_assert(url, 200)
+
+
+@pytest.mark.parametrize(
+    "url", invest_find_published_translated_urls(INVEST_API_PAGES)
+)
+def test_published_and_translated_invest_pages_should_return_200_new(url):
+    get_and_assert(url, 200)
