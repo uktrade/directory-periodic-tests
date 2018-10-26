36cdd764f75fc5bfb93b04a296ab8e2ad415c1a8
kowalcj0 <kowalczykjanusz@gmail.com>
2018-10-26 18:56:44 +0100

    print execution times after each distinctive step

--- geckoboard_updater/geckoboard_updater.py
+++ geckoboard_updater/geckoboard_updater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import time
 
 from datetime import date
 
@@ -45,11 +46,18 @@ CIRCLE_CI_CLIENT = circleclient.CircleClient(CIRCLE_CI_API_TOKEN)
 
 if __name__ == "__main__":
     # Geckoboard Datasets (with schemas)
+    start_time = time.time()
     CONTENT_DATASETS = create_datasets(ContentDatasetSchemas, GECKO_CLIENT)
     TOOLS_DATASETS = create_datasets(ToolsDatasetSchemas, GECKO_CLIENT)
+    total_time = int(time.time() - start_time)
+    print(f"Configuring Geckoboard Datasets took: {total_time}s")
 
+    start_time = time.time()
     from jira_results import *
+    total_time = int(time.time() - start_time)
+    print(f"Fetching stats from Jira took: {total_time}s")
 
+    start_time = time.time()
     # CONTENT_DATASETS.BAD_CMS_PAGES_PER_ENVIRONMENT.dataset.post()
     # CONTENT_DATASETS.BAD_LINKS_PER_ENVIRONMENT.dataset.post()
     # CONTENT_DATASETS.PAGE_DIFFS_PER_ENVIRONMENT.dataset.post()
@@ -63,7 +71,10 @@ if __name__ == "__main__":
     CONTENT_DATASETS.TICKETS_ON_BOARD.dataset.post(content_tickets_on_board)
     CONTENT_DATASETS.UNLABELLED_BUGS_IN_BACKLOG.dataset.post(content_unlabelled_bugs_in_backlog)
     CONTENT_DATASETS.UNLABELLED_BUGS_ON_BOARD.dataset.post(content_unlabelled_bugs_on_board)
+    total_time = int(time.time() - start_time)
+    print(f"Pushing Content Team Jira stats to Geckoboard took: {total_time}s")
 
+    start_time = time.time()
     # TOOLS_DATASETS.BAD_CMS_PAGES_PER_ENVIRONMENT.dataset.post()
     # TOOLS_DATASETS.BAD_LINKS_PER_ENVIRONMENT.dataset.post()
     # TOOLS_DATASETS.PAGE_DIFFS_PER_ENVIRONMENT.dataset.post()
@@ -77,23 +88,43 @@ if __name__ == "__main__":
     TOOLS_DATASETS.TICKETS_ON_BOARD.dataset.post(tools_tickets_on_board)
     TOOLS_DATASETS.UNLABELLED_BUGS_IN_BACKLOG.dataset.post(tools_unlabelled_bugs_in_backlog)
     TOOLS_DATASETS.UNLABELLED_BUGS_ON_BOARD.dataset.post(tools_unlabelled_bugs_on_board)
+    total_time = int(time.time() - start_time)
+    print(f"Pushing Tools Team Jira stats to Geckoboard took {total_time}s")
 
+    start_time = time.time()
     from circleci_results import *
+    total_time = int(time.time() - start_time)
+    print(f"Fetching test results from CircleCI took {total_time}s")
 
+    start_time = time.time()
     TOOLS_DATASETS.LOAD_TESTS_STAGE_CMS_RESULT_DISTRIBUTION.dataset.post(load_test_stage_cms_result_distribution)
     TOOLS_DATASETS.LOAD_TESTS_STAGE_FAB_RESULT_DISTRIBUTION.dataset.post(load_test_stage_fab_result_distribution)
     TOOLS_DATASETS.LOAD_TESTS_STAGE_FAS_RESULT_DISTRIBUTION.dataset.post(load_test_stage_fas_result_distribution)
     TOOLS_DATASETS.LOAD_TESTS_STAGE_INVEST_RESULT_DISTRIBUTION.dataset.post(load_test_stage_invest_result_distribution)
 
+    time.sleep(5)
+
     TOOLS_DATASETS.LOAD_TESTS_STAGE_CMS_RESULT_REQUESTS.dataset.post(load_test_stage_cms_result_requests)
     TOOLS_DATASETS.LOAD_TESTS_STAGE_FAB_RESULT_REQUESTS.dataset.post(load_test_stage_fab_result_requests)
     TOOLS_DATASETS.LOAD_TESTS_STAGE_FAS_RESULT_REQUESTS.dataset.post(load_test_stage_fas_result_requests)
     TOOLS_DATASETS.LOAD_TESTS_STAGE_INVEST_RESULT_REQUESTS.dataset.post(load_test_stage_invest_result_requests)
+    total_time = int(time.time() - start_time)
+    print(f"Pushing Tests Results to Geckoboard took: {total_time}s")
 
+    time.sleep(5)
+
+    start_time = time.time()
     push_directory_service_build_results(
         CIRCLE_CI_CLIENT, GECKOBOARD_PUSH_URL, GECKOBOARD_API_KEY,
         GECKOBOARD_TEST_RESULTS_WIDGET_KEY)
+    total_time = int(time.time() - start_time)
+    print(f"Pushing Build results for Directory Services to Geckoboard took {total_time}s")
 
+    time.sleep(5)
+
+    start_time = time.time()
     push_directory_tests_results(
         CIRCLE_CI_CLIENT, GECKOBOARD_PUSH_URL, GECKOBOARD_API_KEY,
         GECKOBOARD_DIRECTORY_TESTS_RESULTS_WIDGET_KEY)
+    total_time = int(time.time() - start_time)
+    print(f"Pushing test results for Directory Tests to Geckoboard took {total_time}s")
